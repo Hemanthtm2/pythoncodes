368dab55fc633ca82c56ac8a6096e126da3f33cd
Hemanthtm2 <hemanthtm2@gmail.com>
2017-11-01 20:52:55 +0530

    function test

---
+++ line.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python 
+
+
+def newLine():
+  print 
+
+def threeLine():
+ newLine()
+ newLine()
+ newLine()
+
+
+def nineLine():
+  threeLine()
+  threeLine()
+  threeLine()
+
+print "First line"
+newLine()
+print "Second line"
+newLine()
+print "Three Line"
+
+threeLine()
+
+print "Fourthline"
+newLine()
+
+print "Fifth line"
+nineLine()
+
+print "Sixth line"
