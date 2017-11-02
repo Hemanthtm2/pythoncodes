b8357c5332dd9160b626f78c945e9f2f659a8cef
Hemanthtm2 <hemanthtm2@gmail.com>
2017-11-02 17:35:06 +0530

    parity check

---
+++ parity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python 
+
+
+def checkParity(x):
+  
+  if  int(x)%2 == 0:
+    print "Value is Even"
+
+  else:
+    print "Value is Odd"
+
+
+n=raw_input("Enter the value\n")
+
+checkParity(n)
+
+
+
+
+
+
