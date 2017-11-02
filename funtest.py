33f7678bf0edf4739a1421958069379488651c11
Hemanthtm2 <hemanthtm2@gmail.com>
2017-11-02 13:20:23 +0530

    functions understanding

--- funtest.py
+++ funtest.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python 
 
 #Testingfuctions
-def printTwice(bruce):
+def printTwice(bruce): #parameter
   print bruce,bruce
 
+def catTwice(one,two):
+
+  cat=one+two
+
+  printTwice(cat)
 
 
-printTwice('Python')
+printTwice('Python')     #arguments->
 printTwice('Python'*4)
 
 h="Hi Iam Hemanth"
 
 printTwice(h)
+
+
+cha1="Iam"
+cha2="Hemanth"
+
+catTwice(cha1,cha2)
---
+++ funtest2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python 
+
+def printTw(test):
+   print test,test
+
+def addFun(a,b):
+  c=a+b
+  print c
+  printTw(c)
+one=1
+two=5
+addFun(1,5)
+
+on="Hemanth"
+to="Varada"
+addFun(on,to)
