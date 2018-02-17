ffb2888c81595dbe7ee5a015f20090c1c1a73cd7
Hemanthtm2 <hemanthtm2@gmail.com>
2018-02-17 23:51:49 +0530

    adding suffix to string

---
+++ strops.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+fruits="banana"
+letter=fruits[1]
+print letter
+
+print len(fruits)
+print fruits[-1]
---
+++ strrev.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python 
+
+chart="Apples"
+def strRev(chart):
+
+ for char in chart:
+
+   print len(chart) -1
+
+print strRev(chart)
